fd67d49acc85494b12295177f925f82b03302715
sc1611 <64964968+sc1611@users.noreply.github.com>
2022-08-25 00:52:42 +0530

    correct build commit

--- src/test/java/com/example/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/jenkins/api/SpringJenkinsApplicationTests.java
@@ -13,7 +13,7 @@ class SpringJenkinsApplicationTests {
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
-	 contextLoads() {
+	void contextLoads() {
 		logger.info("Test case executing...");
 		logger.info("Test case executing second log statement...");
 		assertEquals(true, true);
