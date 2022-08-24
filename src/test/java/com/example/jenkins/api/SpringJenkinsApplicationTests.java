b5edba2e68e299942b3e453d008db35a3b780b70
sc1611 <64964968+sc1611@users.noreply.github.com>
2022-08-25 00:51:47 +0530

    wrong build commit

--- src/test/java/com/example/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/jenkins/api/SpringJenkinsApplicationTests.java
@@ -13,7 +13,7 @@ class SpringJenkinsApplicationTests {
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
-	void contextLoads() {
+	 contextLoads() {
 		logger.info("Test case executing...");
 		logger.info("Test case executing second log statement...");
 		assertEquals(true, true);
